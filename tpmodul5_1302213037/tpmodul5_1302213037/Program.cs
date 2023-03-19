b231e6b88380b4a581128b40b8816c15f69b9283
MrLI0N7 <93116969+MrLI0N7@users.noreply.github.com>
2023-03-19 16:17:18 +0800

    menambahkan class DataGeneric

--- tpmodul5_1302213037/tpmodul5_1302213037/Program.cs
+++ tpmodul5_1302213037/tpmodul5_1302213037/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using tpmodul5_1302213037;
+
+Console.WriteLine("WELCOME TO TP MOD5\n");
+Console.WriteLine("Silakan input NIM: ");
+string NIM = Console.ReadLine();
+DataGeneric<string> dataBaru = new DataGeneric<string>(NIM);
+dataBaru.PrintData();
+
